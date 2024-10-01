1a3a95f6e546f210da2fe223696a1353d3f25792
Lalit Kumar <lalit7142@gmail.com>
2024-10-01 16:33:10 +0530

    batman

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import Ripple from "@/Components/ui/ripple";
+import Landing from "./Components/Landing";
+
+const App = () => {
+  return (
+    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+      {/* <Ripple /> */}
+      <Landing />
+    </div>
+  );
+};
+
+export default App;
