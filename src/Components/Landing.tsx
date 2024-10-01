53b5c5fdeef983fd131269f0c30f38bbb42579ed
Lalit Kumar <lalit7142@gmail.com>
2024-10-01 16:44:38 +0530

    checking breakpoints

--- src/Components/Landing.tsx
+++ src/Components/Landing.tsx
@@ -93,7 +93,8 @@ const Landing = () => {
         </div>
       </div>
       {/* main */}
-      <div className="container   px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      {/* <div className="container px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center"> */}
+      <div className="container px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
         <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
           <h1 className="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">
             Main Hero Message to sell your app
